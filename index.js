f63b5dcd782d25262ae95b385fc2530f6a550329
Payal Gupta <139633883+payalgupta25@users.noreply.github.com>
2024-02-25 22:03:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function artiCursor(){
+    document.getElementsByClassName("lay")[0].style.cursor= "url('fincur.png'), auto";
+}
+
+function prasadCursor(){
+    document.getElementsByClassName("lay")[0].style.cursor= "url('prasad.png'), auto";
+}
+
+// function prasadCursor(){
+//     document.getElementsByTagName("img")[0].style.cursor= "url('prasad.png'), auto";
+// }
+
